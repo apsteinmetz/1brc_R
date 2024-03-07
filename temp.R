77ef92b9148eca23236346d8eae31fc0cad61ad9
Art Steinmetz <apsteinmetz@yahoo.com>
2024-03-06 23:19:19 -0500

    first draft of blog done.  explore data.table more.

--- temp.R
+++ temp.R
@@ -1,29 +1,47 @@
-do_duckdb <- function(use_tidy = TRUE) {
+do_duckdb <- function(df = tidy_df, use_tidy = TRUE) {
    # duckdb_register(con, "duck_df",overwrite = TRUE, orig_df)
    if (use_tidy) {
-      # convert tibble to duckdb table
-      result  <- as_duckplyr_df(tidy_df) %>%
-         group_by(city) |>
-         summarize(
-            high = max(temp),
-            low = min(temp),
-            avg = mean(temp)
-         )
+      if ("duckplyr_df" %in% class(df)) {
+         result <- df |>
+            group_by(city) |>
+            summarize(
+               high = max(temp),
+               low = min(temp),
+               avg = mean(temp)
+            )
+         return(result)
+      } else {
+         result <- as_duckplyr_df(df) |>
+            group_by(city) |>
+            summarize(
+               high = max(temp),
+               low = min(temp),
+               avg = mean(temp)
+            )
+         return(result)
+      }
    } else {
       con <- dbConnect(duckdb::duckdb())
-      duckdb_register(con, "duck_df",overwrite = TRUE, tidy_df)
+      duckdb_register(con, "duck_df", overwrite = TRUE, df)
       # achieve the same result with dbGetQuery
       result <- dbGetQuery(
          con,
          "SELECT city, AVG(temp) as avg, MIN(temp) as low, MAX(temp) as high FROM duck_df GROUP BY city"
       )
-      result <- result %>%
-         as_tibble()
-      dbDisconnect(con,shutdown = TRUE)
+      result
+      dbDisconnect(con, shutdown = TRUE)
       rm(con)
    }
    return(result)
 }
 
-do_duckdb(use_tidy = TRUE)
-do_duckdb(use_tidy = FALSE)
+duck_df <- as_duckplyr_df(tidy_df)
+
+bm_duckdb <- microbenchmark(do_duckdb(tidy_df,use_tidy = FALSE),
+                            do_duckdb(tidy_df,use_tidy = TRUE),
+                            do_duckdb(duck_df,use_tidy = TRUE),
+                            times = 1, unit="seconds")
+bm_duckdb
+
+
+microbenchmark(df <- tidy_df,df <- as_duckplyr_df(tidy_df),times = 100, unit="microseconds")
