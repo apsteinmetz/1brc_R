b6c180aef0415b24abbb8edabdb8e2417023ed06
Art Steinmetz <apsteinmetz@yahoo.com>
2024-03-08 14:13:37 -0500

    cleaning up.

--- temp.R
+++ temp.R
@@ -1,47 +1,74 @@
-do_duckdb <- function(df = tidy_df, use_tidy = TRUE) {
-   # duckdb_register(con, "duck_df",overwrite = TRUE, orig_df)
-   if (use_tidy) {
-      if ("duckplyr_df" %in% class(df)) {
-         result <- df |>
-            group_by(city) |>
-            summarize(
-               high = max(temp),
-               low = min(temp),
-               avg = mean(temp)
-            )
-         return(result)
-      } else {
-         result <- as_duckplyr_df(df) |>
-            group_by(city) |>
-            summarize(
-               high = max(temp),
-               low = min(temp),
-               avg = mean(temp)
-            )
-         return(result)
+bench <- function (..., list = NULL, times = 100L, unit = NULL, check = NULL,
+          control = list(), setup = NULL)
+{
+   stopifnot(times == as.integer(times))
+   if (!missing(unit) && !is.null(unit))
+      stopifnot(is.character(unit), length(unit) == 1L)
+   unit <- "seconds"
+   control[["warmup"]] <- coalesce(control[["warmup"]], 2^18L)
+   control[["order"]] <- coalesce(control[["order"]], "random")
+   stopifnot(as.integer(control$warmup) == control$warmup)
+   exprs <- c(as.list(match.call(expand.dots = FALSE)$...),
+              list)
+   nm <- names(exprs)
+   exprnm <- sapply(exprs, function(e) paste(deparse(e), collapse = " "))
+   if (is.null(nm))
+      nm <- exprnm
+   else nm[nm == ""] <- exprnm[nm == ""]
+   names(exprs) <- nm
+   env <- parent.frame()
+   setup <- substitute(setup)
+   if (!is.null(check)) {
+      setupexpr <- as.expression(setup)
+      checkexprs <- lapply(exprs, function(e) c(setupexpr,
+                                                e))
+      values <- lapply(checkexprs, eval, env)
+      if (is.character(check) && isTRUE(check == "equal")) {
+         check <- function(values) {
+            all(sapply(values[-1], function(x) isTRUE(all.equal(values[[1]],
+                                                                x))))
+         }
       }
-   } else {
-      con <- dbConnect(duckdb::duckdb())
-      duckdb_register(con, "duck_df", overwrite = TRUE, df)
-      # achieve the same result with dbGetQuery
-      result <- dbGetQuery(
-         con,
-         "SELECT city, AVG(temp) as avg, MIN(temp) as low, MAX(temp) as high FROM duck_df GROUP BY city"
-      )
-      result
-      dbDisconnect(con, shutdown = TRUE)
-      rm(con)
+      else if (is.character(check) && isTRUE(check == "equivalent")) {
+         check <- function(values) {
+            all(sapply(values[-1], function(x) isTRUE(all.equal(values[[1]],
+                                                                x, check.attributes = F))))
+         }
+      }
+      else if (is.character(check) && isTRUE(check == "identical")) {
+         check <- function(values) {
+            all(sapply(values[-1], function(x) identical(values[[1]],
+                                                         x)))
+         }
+      }
+      ok <- check(values)
+      if (!isTRUE(ok)) {
+         stop("Input expressions are not equivalent.", call. = FALSE)
+      }
    }
-   return(result)
+   gc(FALSE)
+   o <- if (control$order == "random")
+      sample(rep(seq_along(exprs), times = times))
+   else if (control$order == "inorder")
+      rep(seq_along(exprs), times = times)
+   else if (control$order == "block")
+      rep(seq_along(exprs), each = times)
+   else stop("Unknown ordering. Must be one of 'random', 'inorder' or 'block'.")
+   exprs <- exprs[o]
+   if (anyDuplicated(nm) > 0) {
+      duplicates <- nm[duplicated(nm)]
+      stop("Expression names must be unique. Duplicate expression names: ",
+           paste(duplicates, collapse = ", "))
+   }
+   expr <- factor(nm[o], levels = nm)
+   res <- 1:length(exprs)
+   if (all(is.na(res)))
+      .all_na_stop()
+   res <- data.frame(expr = expr, time = res)
+   class(res) <- c("microbenchmark", class(res))
+   if (!is.null(unit))
+      attr(res, "unit") <- unit
+   res
 }
 
-duck_df <- as_duckplyr_df(tidy_df)
-
-bm_duckdb <- microbenchmark(do_duckdb(tidy_df,use_tidy = FALSE),
-                            do_duckdb(tidy_df,use_tidy = TRUE),
-                            do_duckdb(duck_df,use_tidy = TRUE),
-                            times = 1, unit="seconds")
-bm_duckdb
-
-
-microbenchmark(df <- tidy_df,df <- as_duckplyr_df(tidy_df),times = 100, unit="microseconds")
+bench(do_dplyr)
